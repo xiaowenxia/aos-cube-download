230d9b8fdd19f6ef66d395ceb017d698bb964c1a
chenan.xxw <chenan.xxw@alibaba-inc.com>
2018-10-24 20:28:28 +0800

    修改编译错误

--- get-aoscube-download-today.js
+++ get-aoscube-download-today.js
@@ -1,32 +1,7 @@
-
-/**
- * Copyright 2017, Google, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// [START bigquery_simple_app_all]
-function printResult(rows) {
-    // [START bigquery_simple_app_print]
-    console.log('Query Results:');
-    rows.forEach(function (row) {
-        let url = row['url'];
-        let viewCount = row['view_count'];
-        console.log(`url: ${url}, ${viewCount} views`);
-    });
-    // [END bigquery_simple_app_print]
-}
-
 const http = require("http");
+// Imports the Google Cloud client library
+const BigQuery = require('@google-cloud/bigquery');
+
 function send(date, count){
     console.log(date, count);
     var options = {
@@ -55,9 +30,6 @@ function send(date, count){
       req.write(JSON.stringify({ date: date, count: count }));
       req.end();
 }
-
-// Imports the Google Cloud client library
-const BigQuery = require('@google-cloud/bigquery');
 
 // Creates a client
 const bigquery = new BigQuery({
